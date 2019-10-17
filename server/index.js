8ea1347b0f1b21fc048a6a72b57aab19d81037cb
kraftwerk28 <kefirchik3@gmail.com>
2019-10-17 23:46:37 +0300

    add: recover

--- server/index.js
+++ server/index.js
@@ -27,6 +27,11 @@ const transporter = nodemailer.createTransport({
 const PORT = process.env.PORT ||
   (process.env.NODE_ENV === 'development' ? 8080 : 80)
 
+const url = DEV
+  ? 'http://localhost:8080'
+  : 'https://stark-springs-03070.herokuapp.com'
+  
+  
 const users = {}
 
 const createToken = (length = 16) => crypto.randomBytes(length).toString('hex')
@@ -34,7 +39,8 @@ const createToken = (length = 16) => crypto.randomBytes(length).toString('hex')
 const createUser = password => ({
   password,
   confirmationToken: createToken(),
-  loginTokens: []
+  loginTokens: [],
+  recoverToken: undefined
 })
 
 // const users = new Map([[ 'kefirchik3@gmail.com', '123123' ]])
@@ -45,6 +51,11 @@ const app = express()
 //   resolve(__dirname, 'email-template.html'),
 //   'utf8'
 // )
+
+// app.use((_req, _res, next) => {
+//   console.log('Users', users)
+//   return next()
+// })
 
 app.use(cookieParser())
 app.use(bodyParser.json())
@@ -80,10 +91,6 @@ app.post('/register', (req, res) => {
   }
   console.log(`User ${email} added to registry...`)
   users[email] = createUser(password)
-
-  const url = DEV
-    ? 'http://localhost:8080'
-    : 'https://stark-springs-03070.herokuapp.com'
   const html = `
     <div>
       <a href="${url}/confirm?token=${users[email].confirmationToken}">Підтвердити реєстрацію</a>
@@ -149,3 +156,64 @@ app.get('/confirm', (req, res) => {
 })
 
 app.post('/telegram', ctrlTelegram.sendMsg);
+
+app.post('/recover', (req, res) => {
+  const { email } = req.body
+  if (!users[email]) {
+    res.status(400).set('content-type', 'text/plain').end('Такого користувача немає')
+    return
+  }
+  users[email].recoverToken = createToken()
+  
+  const html = `
+    <div>
+      <a href="${url}/recover?token=${users[email].recoverToken}">
+        Підтвердити реєстрацію
+      </a>
+    </div>
+  `
+  
+  transporter.sendMail(
+    {
+      from: GMAIL_USER,
+      to: email,
+      subject: 'Відновлення пароля',
+      html
+    },
+    (err, info) => {
+      if (err) {
+        console.error(err, 'E-Mail sending failed')
+        return
+      }
+      console.log('E-Mail sent successfully')
+    }
+  )
+})
+
+app.get('/recover', (req, res) => {
+  const { token } = req.query
+  const email = Object.keys(users).find(
+    email => users[email].recoverToken === token
+  )
+  if (email) {
+    console.log(`User ${email} started recovering`)
+    res.status(200).sendFile(resolve('public/recover.html'))
+  } else {
+    res.status(401).end('Wrong recover token')
+  }
+})
+
+app.post('/newpass', (req, res) => {
+  const { token, newpass } = req.body
+  const email = Object.keys(users).find(
+    email => users[email].recoverToken === token
+  )
+  
+  if (email) {
+    users[email].password = newpass
+    users[email].recoverToken = undefined
+    res.status(200).end('Password recovered')
+  } else {
+    res.status(401).end('User doesn\'t exist')
+  }
+})
